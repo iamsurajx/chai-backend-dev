8fcfb3948de24a8adac806b7bc8594b2342088e0
iamsurajx <surajc8800@gmail.com>
2024-03-28 19:37:13 +0530

    commit

--- src/db/index.js
+++ src/db/index.js
@@ -4,9 +4,10 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     console.log(`\n MongoDB connected !! DB HOST:${connectionInstance.connection.host}`);
   } catch (error) {
+    console.log("May be TypoError in naming MONGODB_URI or MONGODB_URL");
     console.log("MONGODB Connection FAILED", error)
     process.exit(1)
   }
